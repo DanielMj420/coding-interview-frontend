b8f6993d972ac79a04f188058318b7d1216571cb
Daniel Maldonado <dmaldonadoj@outlook.com>
2024-12-12 18:05:34 -0500

    create baseParagraph

---
+++ lib/presentation/core/base_paragraph.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BaseParagraph extends StatelessWidget {
+  final String text;
+  final double? fontSize;
+  final Color? textColor;
+  final bool isSelectable;
+  const BaseParagraph({
+    super.key,
+    this.textColor,
+    this.fontSize,
+    required this.text,
+    this.isSelectable = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return isSelectable
+        ? SelectableText(
+            text,
+            style: GoogleFonts.poppins(
+              fontSize: fontSize ?? 14.sp,
+              color: textColor ?? Colors.black,
+            ),
+          )
+        : Text(
+            text,
+            style: GoogleFonts.poppins(
+              fontSize: fontSize ?? 14.sp,
+              color: textColor ?? Colors.black,
+            ),
+          );
+  }
+}
